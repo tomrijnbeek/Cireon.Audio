0797eae520a7650e211165ac67cb8661222c51e9
Tom <tomrijnbeek@gmail.com>
2014-02-18 21:56:26 +0100

    Fixed the song to use treams in a proper way, solving a few bugs.

--- src/Cireon.Audio/Song.cs
+++ src/Cireon.Audio/Song.cs
@@ -1,11 +1,15 @@
-﻿namespace Cireon.Audio
+﻿using System;
+
+namespace Cireon.Audio
 {
     /// <summary>
     /// Wrapper class for background music.
     /// </summary>
     public sealed class Song
     {
-        private readonly OggStream stream;
+        private readonly string file;
+        private readonly bool prepareBuffer;
+        private OggStream stream;
 
         /// <summary>
         /// True if the stream has been initialised and ready to play.
@@ -25,42 +29,83 @@
             get { return this.finished; }
         }
 
+        private float volume;
         /// <summary>
         /// The volume at which the song is played.
         /// </summary>
         public float Volume
         {
-            get { return this.stream.Volume; }
-            set { this.stream.Volume = value; }
+            get { return this.volume; }
+            set
+            {
+                this.volume = value;
+                if (this.stream != null)
+                    this.stream.Volume = this.volume;
+            }
         }
 
+        private float pitch;
         /// <summary>
         /// The pitch at which the song is played.
         /// </summary>
         public float Pitch
         {
-            get { return this.stream.Pitch; }
-            set { this.stream.Pitch = value; }
+            get { return this.pitch; }
+            set
+            {
+                this.pitch = value;
+                if (this.stream != null)
+                    this.stream.Pitch = this.pitch;
+            }
         }
 
+        private bool looping;
         /// <summary>
         /// Whether the song should be looping.
         /// </summary>
         public bool Looping
         {
-            get { return this.stream.IsLooped; }
-            set { this.stream.IsLooped = value; }
+            get { return this.looping; }
+            set
+            {
+                this.looping = value;
+                if (this.stream != null)
+                    this.stream.IsLooped = this.looping;
+            }
         }
 
         /// <summary>
         /// Creates a new song from a file.
         /// </summary>
         /// <param name="file">The filename of the ogg-file that contains the music.</param>
-        public Song(string file)
+        /// <param name="prepareBuffer">Whether the song should prebuffer the first few seconds of music.</param>
+        public Song(string file, bool prepareBuffer = false)
         {
-            this.stream = new OggStream(file);
+            this.file = file;
+            this.prepareBuffer = prepareBuffer;
+
+            if (this.prepareBuffer)
+                this.initialiseStream();
+        }
+
+        private void initialiseStream()
+        {
+            if (this.stream != null)
+                throw new InvalidOperationException("You can not have two streams running at the same time.");
+
+            this.stream = new OggStream(this.file);
             this.stream.Prepare();
-            this.stream.Finished += (sender, args) => this.finished = !this.Looping;
+            this.stream.Finished += this.onStreamFinish;
+        }
+
+        private void onStreamFinish(object sender, EventArgs e)
+        {
+            this.finished = true;
+            this.stream.Dispose();
+            this.stream = null;
+
+            if (this.prepareBuffer)
+                this.initialiseStream();
         }
 
         /// <summary>
@@ -69,6 +114,9 @@
         public void Play()
         {
             this.finished = false;
+
+            if (this.stream == null)
+                this.initialiseStream();
             this.stream.Play();
         }
 
@@ -89,12 +137,22 @@
         }
 
         /// <summary>
+        /// Prepares the buffer for playing (if it isn't already).
+        /// </summary>
+        public void Prepare()
+        {
+            if (this.stream != null)
+                this.initialiseStream();
+        }
+
+        /// <summary>
         /// Stops the song and frees up the allocated resources.
         /// </summary>
         public void Dispose()
         {
             this.Stop();
-            this.stream.Dispose();
+            if (this.stream != null)
+             this.stream.Dispose();
         }
     }
 }
