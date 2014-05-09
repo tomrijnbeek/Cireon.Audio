fc599c2dbddee32685f148a2eab3063f0ae22b49
Tom <tomrijnbeek@gmail.com>
2014-05-09 15:04:59 +0200

    Added a `Call` function to the `ALHelper` that can be used to make the code less ugly because it replaces the `ALHelper.Check` calls.

--- src/Cireon.Audio/ALHelper.cs
+++ src/Cireon.Audio/ALHelper.cs
@@ -19,5 +19,27 @@ namespace Cireon.Audio
                 throw new InvalidOperationException(AL.GetErrorString(error));
 #endif
         }
+
+        /// <summary>
+        /// Calls a function and then checks for an OpenAL error.
+        /// </summary>
+        /// <param name="function">The function to be called.</param>
+        public static void Call(Action function)
+        {
+            function();
+            ALHelper.Check();
+        }
+
+        /// <summary>
+        /// Calls a function and then checks for an OpenAL error.
+        /// </summary>
+        /// <typeparam name="TParameter">The type of the parameter of the function.</typeparam>
+        /// <param name="function">The function to be called.</param>
+        /// <param name="parameter">The parameter to be passed to the function.</param>
+        public static void Call<TParameter>(Action<TParameter> function, TParameter parameter)
+        {
+            function(parameter);
+            ALHelper.Check();
+        }
     }
 }
